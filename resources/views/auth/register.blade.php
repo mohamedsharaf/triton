b8df81b89364346522c9bd73a658b06265a76135
Juan Victor Serrudo Chavez <juan.vsc@gmail.com>
2018-05-11 16:08:06 -0400

    Registro de nuevos usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
         <img alt="image" class="img-circle" width="180" src="{!! asset('image/logo/logo_fge_256.png') !!}" />
     </div>
 
-    <h3 class="text-white">Registrarse</h3>
+    {{-- <h3 class="text-white">Registrarse</h3> --}}
     <p class="text-white">
         Escriba su información.
     </p>
@@ -23,12 +23,52 @@
     <form class="m-t" role="form" method="POST" action="{{ route('register') }}">
         {{ csrf_field() }}
 
+        <div class="form-group{{ $errors->has('n_documento') ? ' has-error' : '' }}">
+            <input id="n_documento" type="text" class="form-control" name="n_documento" placeholder="Cédula de identidad" value="{{ old('n_documento') }}" required autofocus>
+
+            @if ($errors->has('n_documento'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('n_documento') }}</strong>
+                </span>
+            @endif
+        </div>
+
         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <input id="name" type="text" class="form-control" name="name" placeholder="Nombre" value="{{ old('name') }}" required autofocus>
+            <input id="name" type="text" class="form-control" name="name" placeholder="Nombre(s)" value="{{ old('name') }}" required autofocus>
 
             @if ($errors->has('name'))
                 <span class="help-block">
                     <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('ap_paterno') ? ' has-error' : '' }}">
+            <input id="ap_paterno" type="text" class="form-control" name="ap_paterno" placeholder="Apellido paterno" value="{{ old('ap_paterno') }}" required autofocus>
+
+            @if ($errors->has('ap_paterno'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('ap_paterno') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('ap_materno') ? ' has-error' : '' }}">
+            <input id="ap_materno" type="text" class="form-control" name="ap_materno" placeholder="Apellido materno" value="{{ old('ap_materno') }}" required autofocus>
+
+            @if ($errors->has('ap_materno'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('ap_materno') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('f_nacimiento') ? ' has-error' : '' }}">
+            <input id="f_nacimiento" type="text" class="form-control" name="f_nacimiento" placeholder="Fecha de nacimiento" value="{{ old('f_nacimiento') }}" required autofocus>
+
+            @if ($errors->has('f_nacimiento'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('f_nacimiento') }}</strong>
                 </span>
             @endif
         </div>
